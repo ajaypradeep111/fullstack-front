545974a8eb299f179825bec495435e35aaa208df
ajaypradeep111 <ajaypradeepsiraparapu@gmail.com>
2026-01-11 12:51:08 +0530

    Add ViewUser component and update routing; enhance Home component with user deletion functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Home() {
     const [users, setUsers] = React.useState([]);
+
+    const {id} = useParams();
 
     useEffect(() => {
         loadUsers();
@@ -13,6 +15,11 @@ export default function Home() {
         const result = await axios.get("http://localhost:8080/users");
         setUsers(result.data);
     };
+
+    const deleteUser = async (id) => {
+        await axios.delete(`http://localhost:8080/user/${id}`);
+        loadUsers();
+    }
     return (
         <div className='container'>
             <div className='py-4'>
@@ -35,9 +42,9 @@ export default function Home() {
                             <td>{user.username}</td>
                             <td>{user.email}</td>
                             <td>
-                                <button className='btn btn-primary mx-2'>View</button>
+                                <Link className='btn btn-primary mx-2' to={`/viewuser/${user.id}`}>View</Link>
                                 <Link className='btn btn-outline-primary mx-2' to={`/edituser/${user.id}`}>Edit</Link>
-                                <button className='btn btn-danger mx-2'>Delete</button>
+                                <button className='btn btn-danger mx-2' onClick={() => deleteUser(user.id)}>Delete</button>
                             </td>
                             </tr>
                             ))
